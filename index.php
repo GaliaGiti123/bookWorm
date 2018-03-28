51e8584d568b27eaa74c04c5326c3663328ef109
Galia Rabbani <gitigalia@gmail.com>
2018-03-28 23:01:46 +0600

    initial commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Online Bookshop</title>
+	<link rel="stylesheet" type="text/css" href="./src/css/books.css">
+	
+</head>
+<body>
+	<ul>
+		<li><a>Home</a></li>
+		<li><a>About</a>
+
+			<ul>
+				<li><a>Our Team</a></li>
+				<li><a>Mission & Vission</a></li>
+				<li><a>Resource</a></li>
+
+			
+			</ul>
+		</li>
+		<li><a>Book List</a>
+			<ul>
+				<li><a>Story Books</a></li>
+				<li><a>Novel</a></li>
+				<li><a>Articles</a></li>
+				<li><a>Poems</a></li>
+				<li><a>English Books</a></li>
+			</ul>
+		</li>
+		<li><a>Contacts</a>
+			<ul>
+				<li><a>Maps</a></li>
+				<li><a>Direction</a></li>
+			</ul>
+		</li>
+		<li><a>Help</a></li>
+
+	</ul>
+	
+
+</body>
+</html>
